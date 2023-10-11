2e8d9d0354af21b029a1cf0083dc6ebfc3491d6c
M.V.N.S.Prudhvi <34057003+prudhvimvns@users.noreply.github.com>
2023-10-11 12:33:14 -0700

    updated code-2

--- app.py
+++ app.py
@@ -15,42 +15,51 @@ uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 # Add a text input for the OpenAI API key
 openai_api_key = st.text_input("Enter your OpenAI API Key")
 
-def extract_and_chat(pdf_path, openai_key, query):
-    os.environ['OPENAI_API_KEY'] = openai_key
-    pdfreader = PdfReader(pdf_path)
-    raw_text = ''
-    for i, page in enumerate(pdfreader.pages):
-        content = page.extract_text()
-        if content:
-            raw_text += content
+def extract_text_from_pdf(pdf_file):
+    try:
+        pdf_reader = PdfReader(pdf_file)
+        text = ''
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        return text
+    except Exception as e:
+        st.error(f"Error extracting text from PDF: {str(e)}")
+        return None
 
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=800,
-        chunk_overlap=200,
-        length_function=len,
-    )
-    texts = text_splitter.split_text(raw_text)
-
-    embeddings = OpenAIEmbeddings()
-    document_search = FAISS.from_texts(texts, embeddings)
-
-    from langchain.chains.question_answering import load_qa_chain
-    from langchain.llms import OpenAI
-
-    chain = load_qa_chain(OpenAI(), chain_type="stuff")
-
-    docs = document_search.similarity_search(query)
-    result = chain.run(input_documents=docs, question=query)
-    return result
+def chat_with_pdf(text, openai_key, query):
+    try:
+        os.environ['OPENAI_API_KEY'] = openai_key
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=800,
+            chunk_overlap=200,
+            length_function=len,
+        )
+        texts = text_splitter.split_text(text)
+        embeddings = OpenAIEmbeddings()
+        document_search = FAISS.from_texts(texts, embeddings)
+        from langchain.chains.question_answering import load_qa_chain
+        from langchain.llms import OpenAI
+        chain = load_qa_chain(OpenAI(), chain_type="stuff")
+        docs = document_search.similarity_search(query)
+        result = chain.run(input_documents=docs, question=query)
+        return result
+    except Exception as e:
+        st.error(f"Error while processing the PDF: {str(e)}")
+        return None
 
 # Add a text input for user questions
 user_question = st.text_input("Ask a question")
 
 if st.button("Chat"):
     if uploaded_file is not None and openai_api_key and user_question:
-        result = extract_and_chat(uploaded_file, openai_api_key, user_question)
-        st.write("Answer:", result)
+        pdf_text = extract_text_from_pdf(uploaded_file)
+        if pdf_text:
+            result = chat_with_pdf(pdf_text, openai_api_key, user_question)
+            if result:
+                st.write("Answer:", result)
+        else:
+            st.warning("Unable to extract text from the PDF. Please check the PDF file.")
     else:
         st.warning("Please upload a PDF file, provide an OpenAI API Key, and enter a question.")
 
